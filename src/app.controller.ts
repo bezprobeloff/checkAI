8df70ec26274a601bb11d54adbf3d01ca5c1b92e
Mikhail Belov <mikhail.belov@ic-group.ru>
2025-07-07 09:17:10 +0300

    feat: add axios && endpoints test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,16 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('test')
+  async getStatusAvtomat() {
+    const test = await this.appService.getStatusAvtomat();
+    return test;
+  }
+
+  // здесь переберешь массив вызовов и узнаешь конечный результат
+  @Get('all')
+  getAllStatuses() {
+    return 'all';
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,35 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class AppService {
+  constructor(private httpService: HttpService) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getStatusAvtomat() {
+    const url = 'https://api.avtomat.dev.x840.ru/auth';
+    // TODO подставь потом сюда данные через ENV
+    const body = { phone: '', password: '' };
+
+    try {
+      const response = await firstValueFrom(this.httpService.post(url, body));
+      console.log(response.data);
+      // здесь длиннный код где вставляешь токен и потом подключаешься куда-то и тд
+      // или статусы
+
+      return {
+        success: true,
+        name: url,
+      };
+    } catch {
+      return {
+        success: false,
+        name: url,
+      };
+    }
   }
 }
